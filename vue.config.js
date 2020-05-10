1ed83efd929ce3397739783e9661da9152e0baec
ilvoeqq2 <nice2021usa>
2020-05-10 09:43:43 +0800

    输出路径改成相对路径，修复线上资源文件引用不到

--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,7 @@ module.exports = {
   // publicPath: process.env.NODE_ENV === 'production'
   //   ? '/admin/'
   //   : '/'
+  // outputDir: __dirname +"/dist",
+
+  baseUrl: "./",   //    这里需要加这个配置   如果文件放在域名 的一个子目录下，需要用相对路径
 }
