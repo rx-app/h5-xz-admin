04bf87e7246a7f978a05afa870c9b5cc943b97ed
ZHL-ZHL <zhl5632@163.com>
2020-06-01 10:56:42 +0800

    用户名

--- src/views/AdminUserEdit.vue
+++ src/views/AdminUserEdit.vue
@@ -10,7 +10,7 @@
       @submit.native.prevent="save()"
     >
       <el-form-item label="用户名" prop="username">
-        <el-input v-model="model.username"></el-input>
+        <el-input v-model="model.username" :disabled="id?true:false"></el-input>
       </el-form-item>
       <el-form-item label="密码" prop="password">
         <el-input type="password" v-model="model.password"></el-input>
